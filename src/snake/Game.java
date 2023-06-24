8a73bcb996b2781830e9c034cb689e22b4867a52
Rowan Twilley <91057630+rowantwilley16@users.noreply.github.com>
2023-06-24 15:48:27 +0200

    final

--- src/snake/Game.java
+++ src/snake/Game.java
@@ -1,19 +1,11 @@
 package snake;
 
-import javax.swing.*; 
-import java.awt.*; 
-
 public class Game {
-	
-	public static final int DIRECTION_NONE = 0,
-	DIRECTION_RIGHT = 1, DIRECTION_LEFT = -1,
-	DIRECTION_UP = 2, DIRECTION_DOWN = -2;
-	
-	static final int WIDTH = 500; 
-	static final int HEIGHT = 500;
-	static final int UNIT_SIZE = 20; 
-	static final int NUMBER_OF_UNITS = (WIDTH *HEIGHT)/(UNIT_SIZE * UNIT_SIZE); 
-	
+
+	public static final int DIRECTION_NONE
+		= 0,
+		DIRECTION_RIGHT = 1, DIRECTION_LEFT = -1,
+		DIRECTION_UP = 2, DIRECTION_DOWN = -2;
 	private Snake snake;
 	private Board board;
 	private int direction;
@@ -57,11 +49,11 @@ public class Game {
 	// and accept user input from the Keyboard.
 	public void update()
 	{
-		System.out.println("Going to update the game"); //game ticks
-		
+		System.out.println("Going to update the game");
 		if (!gameOver) {
 			if (direction != DIRECTION_NONE) {
-				Cell nextCell = getNextCell(snake.getHead());
+				Cell nextCell
+					= getNextCell(snake.getHead());
 
 				if (snake.checkCrash(nextCell)) {
 					setDirection(DIRECTION_NONE);
@@ -69,7 +61,8 @@ public class Game {
 				}
 				else {
 					snake.move(nextCell);
-					if (nextCell.getCellType() == CellType.FOOD) {
+					if (nextCell.getCellType()
+						== CellType.FOOD) {
 						snake.grow();
 						board.generateFood();
 					}
@@ -102,63 +95,23 @@ public class Game {
 		return nextCell;
 	}
 
-	
-	public void draw(Graphics graphics) {
-		
-		graphics.setColor(new Color(210,115,90));
-		
-		for (int row = 0 ; row < WIDTH ; row ++ ) {
-			for(int col = 0; col < HEIGHT; col ++) {
-				if (board.getCells()[col][row].getCellType() == CellType.EMPTY) {
-					graphics.setColor(Color.DARK_GRAY);
-					graphics.fillRect(row,col, UNIT_SIZE, UNIT_SIZE);
-				}else if (board.getCells()[col][row].getCellType() == CellType.FOOD) {
-					graphics.setColor(Color.RED);
-					graphics.fillRect(row,col, UNIT_SIZE, UNIT_SIZE);
-				}else if(board.getCells()[col][row].getCellType() == CellType.SNAKE_NODE) {
-					graphics.setColor(Color.GREEN);
-					graphics.fillRect(row,col, UNIT_SIZE, UNIT_SIZE);
-				}else {
-					System.out.println("error drawing the scene!");
-				}
-					
-			}
-		}
-			
-		
-	}
 	public static void main(String[] args)
 	{
-		//setup the GUI 
-		JFrame f = new JFrame();
-		f.setSize(WIDTH, HEIGHT);
-		f.setFocusable(true);
-		f.setVisible(true);
-		f.setTitle("Snake-Game-in-Java");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setResizable(false);
-		f.setLocationRelativeTo(null);
-		
-		
+
 		System.out.println("Going to start game");
 
 		Cell initPos = new Cell(0, 0);
 		Snake initSnake = new Snake(initPos);
-		Board board = new Board(WIDTH, HEIGHT);
+		Board board = new Board(10, 10);
 		Game newGame = new Game(initSnake, board);
-		
-		
-		
 		newGame.gameOver = false;
 		newGame.direction = DIRECTION_RIGHT;
 
-		
 		// We need to update the game at regular intervals,
 		// and accept user input from the Keyboard.
 
 		// here I have just called the different methods
 		// to show the functionality
-		
 		for (int i = 0; i < 5; i++) {
 			if (i == 2)
 				newGame.board.generateFood();
